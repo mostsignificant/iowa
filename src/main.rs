6fb08b3777488e08232d69f7954e8159ddf5532e
Christian Göhring <cg@2601.me>
2022-12-04 15:25:13 +0100

    added parameter as key support, corrected cli args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use actix_web::{get, post, web, http::header::ContentType, App, HttpResponse, HttpServer};
+use actix_web::{
+    get, http::header::ContentType, post, web, App, HttpRequest, HttpResponse, HttpServer,
+};
 use clap::Parser;
-use std::collections::{HashMap};
+use std::collections::HashMap;
 use std::sync::Mutex;
 
 /// App state which stores all keys and values.
@@ -10,36 +12,39 @@ struct AppState {
 
 /// Returns a value from the given path.
 #[get("/{key:.*}")]
-async fn get(path: web::Path<(String,)>, data: web::Data<AppState>) -> HttpResponse {
-    let key = path.into_inner().0;
+async fn get(request: HttpRequest, data: web::Data<AppState>) -> HttpResponse {
+    println!("{:?}", request);
+    let key: String = request.uri().to_string();
+
     match data.store.lock().unwrap().get(&key) {
-        Some(value) =>
-            return 
-                HttpResponse::Ok()
-                    .content_type(ContentType::plaintext())
-                    .body(value.to_string()),
-        _ => 
-            return
-                HttpResponse::NotFound()
-                    .content_type(ContentType::plaintext())
-                    .body("key not found"),
+        Some(value) => {
+            return HttpResponse::Ok()
+                .content_type(ContentType::plaintext())
+                .body(value.to_string())
+        }
+        _ => {
+            return HttpResponse::NotFound()
+                .content_type(ContentType::plaintext())
+                .body("key not found")
+        }
     }
 }
 
 /// Sets a value to the given path.
 #[post("/{key:.*}")]
-async fn set(path: web::Path<(String,)>, value: web::Bytes, data: web::Data<AppState>) -> HttpResponse {
-    let key = path.into_inner().0;
-    let value = String::from_utf8(value.to_vec()).unwrap();
+async fn set(request: HttpRequest, post: web::Bytes, data: web::Data<AppState>) -> HttpResponse {
+    println!("{:?}", request);
+
+    let key: String = request.uri().to_string();
+    let value = String::from_utf8(post.to_vec()).unwrap();
+
     match data.store.lock().unwrap().insert(key, value) {
-        None =>
-            HttpResponse::Created()
-                .content_type(ContentType::plaintext())
-                .body("new value inserted"),
-        Some(_) =>
-            HttpResponse::Accepted()
-                .content_type(ContentType::plaintext())
-                .body("value updated"),
+        None => HttpResponse::Created()
+            .content_type(ContentType::plaintext())
+            .body("new value inserted"),
+        Some(_) => HttpResponse::Accepted()
+            .content_type(ContentType::plaintext())
+            .body("value updated"),
     }
 }
 
@@ -48,12 +53,12 @@ async fn set(path: web::Path<(String,)>, value: web::Bytes, data: web::Data<AppS
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Host of the web service
-   #[arg(default_value_t = String::from("0.0.0.0"))]
-   host: String,
+    #[arg(long, default_value_t = String::from("0.0.0.0"))]
+    host: String,
 
-   /// Port of the web service
-   #[arg(default_value_t = 1984)]
-   port: u16,
+    /// Port of the web service
+    #[arg(long, default_value_t = 1984)]
+    port: u16,
 }
 
 /// Main method to instantiate key value store and start http server.
@@ -61,18 +66,12 @@ struct Args {
 async fn main() -> std::io::Result<()> {
     let args = Args::parse();
 
-    let state = 
-        web::Data::new(AppState {
-            store: Mutex::new(HashMap::new()),
-        });
+    let state = web::Data::new(AppState {
+        store: Mutex::new(HashMap::new()),
+    });
 
-    HttpServer::new(move || {
-        App::new()
-            .app_data(state.clone())
-            .service(get)
-            .service(set)
-    })
-    .bind((args.host, args.port))?
-    .run()
-    .await
+    HttpServer::new(move || App::new().app_data(state.clone()).service(get).service(set))
+        .bind((args.host, args.port))?
+        .run()
+        .await
 }
